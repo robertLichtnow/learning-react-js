f92dab56fc62d24937dde594227aa2d8364e96b0
Matheus Robert Lichtnow <matheusrobert8@gmail.com>
2019-04-20 22:47:42 -0300

    Adicionada lista

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,16 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count ? "primary" : "warning";
+    return classes;
   }
 
   formatCount() {
