b968f754e4262984762c5530a60c31a58f5ae593
706684 <matheus.lichtnow@pti.org.br>
2019-04-30 09:06:42 -0300

    Corrigindo objetos passados entre componentes

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -11,15 +11,22 @@ class Counters extends Component {
     ]
   };
 
-handleDelete = () => {
-    console.log('Event Handler Called')
-}
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(
+      counter => counter.id != counterId
+    );
+    this.setState({ counters });
+  };
 
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id}/>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
