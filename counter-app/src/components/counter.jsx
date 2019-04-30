50c41849f854fa57bc892dd0f6de9fd19d974ef9
706684 <matheus.lichtnow@pti.org.br>
2019-04-30 08:58:53 -0300

    Criando e lidando com eventos

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,23 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.value
   };
 
   getBadgeClasses(){
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? 'warning' : 'primary';
+    classes += this.state.value === 0 ? 'warning' : 'primary';
     return classes; 
   }
 
   formatCount(){
-    let {count} = this.state;
-    return count === 0 ? 'Zero' : count;
+    let {value: value} = this.state;
+    return value === 0 ? 'Zero' : value;
   }
 
   handleIncrement = product => {
-    console.log(product);
-    this.setState({count: this.state.count + 1})
+    this.setState({value: this.state.value + 1})
   }
 
   render() {
@@ -26,6 +25,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button onClick={() => this.handleIncrement({id:1})} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
